75bbc580819ae9e505114785b9f6de7c8439053a
JulioCFreitas <juliosparapan2014@gmail.com>
2026-04-12 20:12:57 -0300

    Initial commit - base project structure

---
+++ README.md
@@ -0,0 +1,1 @@
+# Livestock-AI-Platform
---
+++ src/main/java/com/livestock/LivestockAiApplication.java
@@ -0,0 +1,13 @@
+package com.livestock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivestockAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivestockAiApplication.class, args);
+	}
+
+}
