c6979374479e04f5abe4e5da07e9ac6f18d572b8
JulioCFreitas <juliosparapan2014@gmail.com>
2026-05-31 20:52:55 -0300

    Correção BCryptPasswordEncoder

--- src/main/java/com/livestock/adapters/out/security/BCryptPasswordEncoderAdapter.java
+++ src/main/java/com/livestock/adapters/out/security/BCryptPasswordEncoderAdapter.java
@@ -1,10 +1,18 @@
 package com.livestock.adapters.out.security;
 
 import com.livestock.application.ports.PasswordEncoderPort;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
 
-public class BCryptPasswordEncoderAdapter implements PasswordEncoderPort {
+@Component
+public class BCryptPasswordEncoderAdapter
+        implements PasswordEncoderPort {
 
-    private final BCryptPasswordEncoderAdapter encoder = new BCryptPasswordEncoderAdapter();
+    private final BCryptPasswordEncoder encoder;
+
+    public BCryptPasswordEncoderAdapter( BCryptPasswordEncoder encoder) {
+        this.encoder = encoder;
+    }
 
     @Override
     public String encode(String senha) {
